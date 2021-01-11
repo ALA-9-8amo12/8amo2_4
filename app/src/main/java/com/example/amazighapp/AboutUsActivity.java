5400d212c67a75232257b8e8299610fffb7da9b7
rick <1040133@mborijnland.nl>
2021-01-11 15:07:10 +0100

    category selector en over ons

--- app/src/main/java/com/example/amazighapp/AboutUsActivity.java
+++ app/src/main/java/com/example/amazighapp/AboutUsActivity.java
@@ -1,14 +1,22 @@
 package com.example.amazighapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.view.View;
 
 import android.os.Bundle;
 
 public class AboutUsActivity extends AppCompatActivity {
 
+    public void onClick(View view){
+        Intent i = new Intent(this, HomeActivity.class);
+        startActivity(i);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
